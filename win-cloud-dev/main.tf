52bf024d2de9c421f3fd63289c6c6a81abe9f2ae
Rylan McCarty <rylan@protopacer.com>
2024-01-22 22:48:31 -0600

    Fixed bug with subnet configuration and switched to Windows Server 2022

--- win-cloud-dev/main.tf
+++ win-cloud-dev/main.tf
@@ -2,24 +2,29 @@ provider "azurerm" {
   features {}
 }
 
-variable "vm-name" {
+variable "vm_name" {
   description = "VM name and prefix for supporting infrastructure."
   default     = "winvm"
 }
 
-variable "admin-pass" {
+variable "admin_username" {
+  description = "Temporary administrator usermae for Windows VM"
+  type        = string
+}
+
+variable "admin_password" {
   description = "Temporary administrator password for Windows VM"
-  type = string
-  sensitive = true
+  type        = string
+  sensitive   = true
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "rylan_mccarty-${var.vm-name}"
+  name     = "rylan_mccarty-${var.vm_name}"
   location = "East US"
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "${var.vm-name}-vnet"
+  name                = "${var.vm_name}-vnet"
   address_space       = ["172.20.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
@@ -33,27 +38,27 @@ resource "azurerm_subnet" "subnet" {
 }
 
 resource "azurerm_public_ip" "ip" {
-  name                = "${var.vm-name}-ip"
+  name                = "${var.vm_name}-ip"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Dynamic"
 }
 
 resource "azurerm_network_interface" "nic" {
-  name                = "${var.vm-name}-nic"
+  name                = "${var.vm_name}-nic"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = "azurerm_subnet.subnet.id"
+    subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.ip.id
   }
 }
 
 resource "azurerm_network_security_group" "nsg" {
-  name                = "${var.vm-name}-nsg"
+  name                = "${var.vm_name}-nsg"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
@@ -84,12 +89,12 @@ resource "azurerm_network_security_group" "nsg" {
 
 
 resource "azurerm_windows_virtual_machine" "winvm" {
-  name                = var.vm-name
+  name                = var.vm_name
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   size                = "Standard_B2s"
-  admin_username      = "rmccarty"
-  admin_password      = var.admin-password
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
   network_interface_ids = [
     azurerm_network_interface.nic.id,
   ]
@@ -101,16 +106,38 @@ resource "azurerm_windows_virtual_machine" "winvm" {
   }
 
   source_image_reference {
-    publisher = "MicrosoftWindowsDesktop"
-    offer     = "Windows-11"
-    sku       = "win11-22h2-pro"
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2022-datacenter-azure-edition-hotpatch"
     version   = "latest"
   }
 
-  computer_name            = var.vm-name
+  computer_name            = var.vm_name
   enable_automatic_updates = true
-  provision_vm_agent        = true
+  patch_mode = "AutomaticByPlatform"
+  provision_vm_agent       = true
 }
+
+resource "azurerm_dev_test_global_vm_shutdown_schedule" "auto_shutdown" {
+  virtual_machine_id = azurerm_windows_virtual_machine.winvm.id
+  location           = azurerm_resource_group.rg.location
+  enabled            = true
+
+  daily_recurrence_time = "2000"
+  timezone              = "Central Standard Time"
+
+  notification_settings {
+    enabled = false
+  }
+}
+
+# resource "azurerm_virtual_machine_extension" "openssh" {
+#   name                 = "WindowsOpenSSH"
+#   virtual_machine_id   = azurerm_windows_virtual_machine.winvm.id
+#   publisher            = "Microsoft.Azure.OpenSSH"
+#   type                 = "OpenSSH"
+#   type_handler_version = "3.0"
+# }
 
 # Associate the network security group to the subnet
 resource "azurerm_subnet_network_security_group_association" "nsga" {
@@ -118,7 +145,15 @@ resource "azurerm_subnet_network_security_group_association" "nsga" {
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
+# `Dynamic` Public IP addresses aren't allocated until they're attached to a device
+# (e.g., a VM). Instead you can obtain the IP Address once the Public IP has been assigned via
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/data-sources/public_ip
+data "azurerm_public_ip" "ip" {
+  name                = azurerm_public_ip.ip.name
+  resource_group_name = azurerm_windows_virtual_machine.winvm.resource_group_name
+}
+
 output "public_ip_address" {
-  value = azurerm_public_ip.ip.ip_address
+  value = data.azurerm_public_ip.ip.ip_address
 }
 
